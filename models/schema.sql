8f15d5d047865a71614917e1928539c8085bf25c
David Bontigao <bontigaodavid@gmail.com>
2019-10-31 12:08:10 -0500

    finished users api routes and sequelize

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,12 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS hustledb;
+CREATE DATABASE hustledb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE Users(
+    id INTEGER(30),
+    username VARCHAR(20),
+    password VARCHAR(30),
+    email VARCHAR(30),
+    height INTEGER(20),
+    weight INTEGER(20)
+)
+
